53412732c5b62bad54d7ff3964ba8fe578a16161
MINHO47 <kiola03dd@gmail.com>
2023-10-29 05:53:27 +0400

    test Contact

--- airBnB/src/Home.jsx
+++ airBnB/src/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Contact from './Contact';
 
 export default function HomePage() {
     return (
       <div>
-        <h1>Welcome to My React App</h1>
-        <p>This is the home page of our React application.</p>
+        <Contact />
+
       </div>
     );
   }
